14774662837666655faaef40f119bcc251d4472d
Farid Mitri <faridmitri@gmail.com>
2022-03-16 21:00:58 +0200

    Lunch Tray Project

---
+++ Google-Labs/lunch-tray/app/build/generated/source/kapt/debug/com/example/lunchtray/BR.java
@@ -0,0 +1,15 @@
+package com.example.lunchtray;
+
+public class BR {
+  public static final int CheckoutFragment = 1;
+
+  public static final int _all = 0;
+
+  public static final int accompanimentMenuFragment = 2;
+
+  public static final int entreeMenuFragment = 3;
+
+  public static final int sideMenuFragment = 4;
+
+  public static final int viewModel = 5;
+}
